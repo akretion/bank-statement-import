30043c69380f9f37f201501efbe0f09a25c46ace
Renato Lima <renatonlima@gmail.com>
2026-08-22 10:40:13 -0300

    [ADD] account_statement_import_partner_by_vat: inicial commit

---
+++ account_statement_import_partner_by_vat/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Akretion (https://www.akretion.com).
+# @author Renato Lima <renato.lima@akretion.com.br>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Match Statement Line Partner by VAT",
+    "category": "Accounting",
+    "version": "16.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["account_statement_import_base"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "development_status": "Alpha",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "installable": True,
+}
